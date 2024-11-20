4a032d053e1494c67a288ddf4a2ec9fbb915fb50
Kematzy <kematzy@gmail.com>
2024-11-20 16:52:41 +0800

    Update documentation for 'core_ext/blank'
    -- Claude Sonnet 3.5 via Zed

--- lib/core_ext/blank.rb
+++ lib/core_ext/blank.rb
@@ -2,33 +2,33 @@
 
 # reopening Object class
 class Object
-  # An object is blank if it's false, empty, or a whitespace string.
-  # For example, +false+, '', '   ', +nil+, [], and {} are all blank.
+  # Checks if an object is blank or not.
+  # An object is blank if it does not respond to empty? or if it returns false on calling empty?.
+  # If it does not respond to empty?, then it will check if the object is truthy.
   #
-  # This simplifies
+  # This simplifies `!address || address.empty?` to `address.blank?`
   #
-  #   !address || address.empty?
+  # @return [true, false] false if object responds to empty? and
+  #          empty? returns false, true otherwise
   #
-  # to
-  #
-  #   address.blank?
-  #
-  # @return [true, false]
   def blank?
     respond_to?(:empty?) ? !!empty? : !self
   end
-  
-  # An object is present if it's not blank.
+
+  # Returns true if the object is not blank.
+  # An object is considered not blank if it has meaningful content.
   #
-  # @return [true, false]
+  # @return [true, false] true if object is not blank, false otherwise
+  #
   def present?
     !blank?
   end
-  # Returns the receiver if it's present otherwise returns +nil+.
-  # <tt>object.presence</tt> is equivalent to
 
+  # Returns self if the object is present, otherwise returns nil.
   #
-  #    object.present? ? object : nil
+  # @return [Object, nil] self if present?, nil otherwise
+  #
+  # `object.presence` is equivalent to `object.present? ? object : nil`
   #
   # For example, something like
   #
@@ -40,7 +40,6 @@ class Object
   #
   #   region = params[:state].presence || params[:country].presence || 'US'
   #
-  # @return [Object]
   def presence
     self if present?
   end
@@ -108,43 +107,52 @@ end
 class String
   BLANK_RE = /\A[[:space:]]*\z/
 
-  # A string is blank if it's empty or contains whitespaces only:
+  # Returns true if the string consists entirely of whitespace characters
+  #
+  # @return [true, false] true if string is empty or only whitespace, false otherwise
+  #
+  # @example A basic example
   #
   #   ''.blank?       # => true
   #   '   '.blank?    # => true
   #   "\t\n\r".blank? # => true
   #   ' blah '.blank? # => false
   #
-  # Unicode whitespace is supported:
+  # @example Unicode whitespace support
   #
   #   "\u00a0".blank? # => true
   #
-  # @return [true, false]
   def blank?
     BLANK_RE === self
   end
 end
 
 # reopening Numeric class
-class Numeric #:nodoc:
-  # No number is blank:
+class Numeric # :nodoc:
+  # No numeric value is blank:
+  #
+  # @return [false]
+  #
+  # @example A basic example
   #
   #   1.blank? # => false
   #   0.blank? # => false
   #
-  # @return [false]
   def blank?
     false
   end
 end
 
 # reopening Time class
-class Time #:nodoc:
-  # No Time is blank:
+class Time # :nodoc:
+  # No Time is blank
+  #
+  # @return [false]
+  #
+  # @example A basic example
   #
   #   Time.now.blank? # => false
   #
-  # @return [false]
   def blank?
     false
   end
