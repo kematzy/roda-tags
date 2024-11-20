5733eed812e77d230c1a75c604c8caff711fff8d
Kematzy <kematzy@gmail.com>
2024-11-20 16:53:19 +0800

    Update documentation for 'core_ext/hash'

--- lib/core_ext/hash.rb
+++ lib/core_ext/hash.rb
@@ -5,6 +5,16 @@ class Hash
   # remove any other version of #to_html_attributes
   undef_method :to_html_attributes if method_defined?(:to_html_attributes)
 
+  # Converts a hash into HTML attribute string representation.
+  #
+  # @param empties [Boolean, nil] If provided and not nil, will exclude keys with blank values
+  #
+  # @return [String] HTML attribute string with keys sorted alphabetically
+  #
+  # @example
+  #   {class: 'button', id: 'submit'}.to_html_attributes #=> 'class="button" id="submit"'
+  #   {name: nil, id: 'test'}.to_html_attributes(true) #=> 'id="test"'
+  #
   def to_html_attributes(empties = nil)
     hash = dup
     hash.reject! { |_k, v| v.blank? } unless empties.nil?
@@ -17,7 +27,14 @@ class Hash
   end
 
   unless method_defined?(:reverse_merge)
-    # Merges the caller into +other_hash+. For example,
+    # Performs a non-destructive reverse merge with another hash
+    # This is particularly useful for initializing an options hash with default values.
+    #
+    # @param other_hash [Hash] The hash to merge into
+    #
+    # @return [Hash] A new hash with other_hash merged into self
+    #
+    # @example A basic example
     #
     #   options = options.reverse_merge(size: 25, velocity: 10)
     #
@@ -25,15 +42,24 @@ class Hash
     #
     #   options = { size: 25, velocity: 10 }.merge(options)
     #
-    # This is particularly useful for initializing an options hash
-    # with default values.
     def reverse_merge(other_hash)
       other_hash.merge(self)
     end
   end
 
   unless method_defined?(:reverse_merge!)
-    # Destructive +reverse_merge+.
+    # Performs a destructive reverse merge with another hash.
+    # Modifies the original hash by merging in the provided hash while keeping existing values.
+    #
+    # @param other_hash [Hash] The hash to merge into self
+    #
+    # @return [Hash] The modified hash with other_hash merged in
+    #
+    # @example
+    #   options = { size: 10 }
+    #   options.reverse_merge!(size: 25, velocity: 10)
+    #   # => { size: 10, velocity: 10 }
+    #
     def reverse_merge!(other_hash)
       # right wins if there is no left
       merge!(other_hash) { |_key, left, _right| left }
